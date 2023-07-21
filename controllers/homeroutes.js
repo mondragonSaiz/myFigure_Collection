03aaacf8b85e8989e4461f37f51696fe2838ca34
David Mondragon <dragon18saiz@gmail.com>
2023-07-21 15:47:43 -0600

    added logout logic

--- controllers/homeroutes.js
+++ controllers/homeroutes.js
@@ -72,4 +72,14 @@ router.post('/login', async (req, res) => {
   }
 });
 
+router.post('/logout', (req, res) => {
+  console.log('---------- LOGOUT POST REQ RECEIVED');
+  if (req.session.logged_in) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
 module.exports = router;
